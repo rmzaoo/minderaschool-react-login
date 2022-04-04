af8b03cc24f9d92ef8d3876da9d8443d63347867
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2022-04-04 11:34:07 +0100

    lazy loading

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,49 +1,22 @@
-import { lazy, Suspense, useLayoutEffect, useReducer, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import checkLogin from "../../utils/checkLogin";
+import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Gallery = () => {
-  const navigate = useNavigate();
-  const [state, setState] = useState({
-    loading: true,
-    images: [],
-  });
+  let images: any = [];
 
-  useLayoutEffect(() => {
-    const checkLoginStatus = async () => {
-      let res = await checkLogin();
-      if (!res) {
-        navigate("/");
-        alert("You are not logged in");
-      }
-    };
-
-    checkLoginStatus();
-  }, []);
-  return (
-    <div>
-      {state.loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          <div className="gallery-container">
-            {state.images.map((image) => (
-              <div className="gallery-item" key={image}>
-                <LazyLoadImage
-                  src={image}
-                  alt={image}
-                  effect="blur"
-                  width="300px"
-                  height="300px"
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
-  );
+  for (let i = 0; i < 250; i++) {
+    images.push(
+      <LazyLoadImage
+        key={i}
+        src={`https://picsum.photos/2000/1200?random=${i}`}
+        width="500px"
+        height="300px"
+        alt="random img"
+        effect="blur"
+      ></LazyLoadImage>
+    );
+  }
+  return <div>{images}</div>;
 };
 
 export default Gallery;
